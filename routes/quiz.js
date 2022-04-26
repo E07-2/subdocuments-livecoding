4ef42d8aef562b1b72cf5f01cee37ae77e85e46e
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2022-04-26 12:04:15 +0200

    Added more endpoints; update and delete

--- routes/quiz.js
+++ routes/quiz.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // get everything
 router.get("/", async (request, response) => {
-  const data = await Quiz.find();
+  const data = await Quiz.find().lean().limit(1); // speeds up the response to the client
 
   response.send(data);
 });
@@ -46,6 +46,55 @@ router.post("/", async (request, response) => {
     response
       .status(500)
       .send({ message: `Error saving data, ${error.message}` });
+  }
+});
+
+// Here I would like to update the quiz data
+// with PATCH we partially update parts of our data
+router.patch("/:id", async (request, response) => {
+  try {
+    await Quiz.findByIdAndUpdate(request.params.id, {
+      name: request.body.name,
+      numberOfQuestions: request.body.numberOfQuestions,
+      questions: request.body.questions,
+    });
+
+    response.send("Successfully updated!");
+  } catch (error) {
+    response
+      .status(500)
+      .send({ message: `Error updating data, ${error.message}` });
+  }
+});
+
+router.delete("/:id", async (request, response) => {
+  try {
+    await Quiz.findByIdAndDelete(request.params.id);
+
+    response.send(`Quiz ${request.params.id} deleted`);
+  } catch (error) {
+    response
+      .status(500)
+      .send({ message: `Error deleting data, ${error.message}` });
+  }
+});
+
+// With PUT you are REPLACING all the data
+router.put("/:id", async (request, response) => {
+  try {
+    await Quiz.findByIdAndUpdate(request.params.id, {
+      name: request.body.name,
+      numberOfQuestions: request.body.numberOfQuestions,
+      questions: request.body.questions,
+      quizType: request.body.quizType,
+      category: request.body.category,
+    });
+
+    response.send("Successfully updated!");
+  } catch (error) {
+    response
+      .status(500)
+      .send({ message: `Error updating data, ${error.message}` });
   }
 });
 
